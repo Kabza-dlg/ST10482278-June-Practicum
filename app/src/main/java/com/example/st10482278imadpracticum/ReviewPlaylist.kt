78eded605a8c907ef18409937d4ae6d1621e7f0b
RC_Student_lab-assignment <ST10482278@rcconnect.edu.za>
2025-06-19 11:24:55 +0000

    final commit

--- app/src/main/java/com/example/st10482278imadpracticum/ReviewPlaylist.kt
+++ app/src/main/java/com/example/st10482278imadpracticum/ReviewPlaylist.kt
@@ -4,8 +4,11 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
 
 class ReviewPlaylist : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -13,14 +16,29 @@ class ReviewPlaylist : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review_playlist)
 
-        val allCategorySets: MutableList<List<String>> = mutableListOf()
         val MainPage = findViewById<Button>(R.id.MainPage)
         val Exit = findViewById<Button>(R.id.Exit)
-        val Average = findViewById<Button>(R.id.Average)
         val Details = findViewById<Button>(R.id.Details)
 
         Details.setOnClickListener {
+            val playlist = intent.getStringArrayListExtra("category_list")
+
+            if (playlist.isNullOrEmpty()) {
+                Toast.makeText(this , "No items received" , Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val listView = findViewById<ListView>(R.id.PlaylistView)
+
+            val adapter = ArrayAdapter(
+                this ,
+                android.R.layout.simple_list_item_1 ,
+                playlist
+            )
+            listView.adapter = adapter
         }
+
+
 
         MainPage.setOnClickListener {
             val intent = Intent(this , MainActivity::class.java)
@@ -31,8 +49,8 @@ class ReviewPlaylist : AppCompatActivity() {
             finishAffinity()
         }
 
-        Average.setOnClickListener {
-            val
+
+
+            }
         }
-    }
-}
+
